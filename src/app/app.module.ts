ed5bb637aed27d57f9d0ee643b1680f646f1fa2b
Aquiles <aquilesfalcaoo@gmail.com>
2023-09-24 10:12:11 -0300

    feat: atualizado .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './core/components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
   ],
   imports: [
     BrowserModule
